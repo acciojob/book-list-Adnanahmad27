9992508df6dfed10705eb7b5ea093105e4b18baa
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-06 18:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+/* 
+			<tr>
+				<td>${title}</td>
+				<td>${author}</td>
+				<td>${isbn}</td>
+				<td><button type="button" class="delete">x</button></td>
+			</tr>
+*/
+const form = document.getElementById("form");
+const bookList = document.getElementById("book-list");
+
+function deleteButton(e){
+	e.target.parentNode.parentNode.remove();
+}
+
+form.addEventListener("submit" , (e) => {
+	e.preventDefault();
+	let title = form.title.value;
+	let author = form.author.value;
+	let isbn = form.isbn.value;
+
+	const tr = document.createElement("tr");
+	tr.innerHTML = `
+		<td>${title}</td>
+		<td>${author}</td>
+		<td>${isbn}</td>
+		<td><button type="button" class="delete">x</button></td>
+	`;
+
+	// const deleteButton = document.createElement("button");
+    // deleteButton.innerText = "x";
+    // deleteButton.className = "delete";
+    // deleteButton.addEventListener("click", deleteRecord);
+	
+
+	// tr.children[3].appendChild(deleteButton);
+	 tr.children[3].addEventListener("click" , deleteButton)
+	form.reset();
+	bookList.appendChild(tr);
+});
